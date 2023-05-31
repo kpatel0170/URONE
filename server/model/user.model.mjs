6f21c06a2be3986fd4ba77e649f4f1741506c294
Kartik Patel <kartikpatel0170@gmail.com>
2023-05-31 13:41:23 -0600

    added userModel

---
+++ server/model/user.model.mjs
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    profilePicture: String,
+    about: String,
+    followers: [],
+    following: []
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  let user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR));
+
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const user = this;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
