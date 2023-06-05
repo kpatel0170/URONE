8a9c377f657c69d192fc28ec946566ec00ff69a1
Kartik Patel <kartikpatel0170@gmail.com>
2023-06-05 10:46:50 -0600

    Bugfix: added cors

--- server/server.mjs
+++ server/server.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connect from "./utils/connect.mjs";
 import routes from "./routes/routes.mjs";
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const port = process.env.PORT;
 app.listen(port, async () => {
