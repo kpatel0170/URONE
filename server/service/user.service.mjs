a72e34893fc1c04013a35a3f0783c4007f8cb942
Kartik Patel <kartikpatel0170@gmail.com>
2023-06-18 12:54:43 -0600

    added comments on user apis

--- server/service/user.service.mjs
+++ server/service/user.service.mjs
@@ -1,6 +1,14 @@
 import UserModel from '../model/user.model.mjs';
 
 const UserService = {
+
+  /**
+   * getUser()
+   * Retrieves a user by their ID.
+   * @param {id}- The User's ID.
+   * @return {Promise<UserModel>} - A promise that resolves to the retrieved user.
+   * @throw {Error} - If an error occurs while retrieving the user.
+   */
   async getUser(id) {
     try {
       return await UserModel.findById(id);
@@ -9,6 +17,13 @@ const UserService = {
     }
   },
 
+  /**
+   * updateProfile()
+   * Updates the profile of a user with the provided information.
+   * @param {id, name, email, about, type, profilePicture} id - The User's info. to update.
+   * @return {Promise<UserModel>} - A promise that resolves to the updated user.
+   * @throw {Error} - If the user is not found or an error occurs while updating the profile.
+   */
   async updateProfile(id, name, email, about, type, profilePicture) {
     try {
       const updatedUser = await UserModel.findByIdAndUpdate(
